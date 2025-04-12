afc5c5381c7541113e645dff65946dcd3e4b4c98
Sahil-Raval1012 <s224600109@deakin.edu.au>
2025-04-12 15:14:22 +1000

    Committing my changes before pulling

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT||3000;
 app.get('/', (req,res) => {
-  res.send('Welcome to the docker, Task 5.1P - Containerization!');
+  res.send('Welcome to the docker and google cloud, Task 5.2D - Deploying the microservice to the cloud');
 });
 app.listen(PORT, () => {
   console.log(`Hosting is done on the port ${PORT}`);
